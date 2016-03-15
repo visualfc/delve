06338dfade3e5896d8b9ad9949735b6592c17117
Derek Parker <parkerderek86@gmail.com>
2016-03-15 14:18:08 -0700

    wip

--- proc/exec_darwin.c
+++ proc/exec_darwin.c
@@ -9,23 +9,12 @@ fork_exec(char *argv0, char **argv, int size,
 		mach_port_t *exception_port,
 		mach_port_t *notification_port)
 {
-	// Create a pipe so that we know when we have exec'ed. This ensures that control only returns
-	// back to Go-land when we call exec, effectively eliminating a race condition between launching the new
-	// process and trying to read its memory.
-	int wfd[2];
-	if (pipe(wfd) < 0) return -1;
-	if (fcntl(wfd[0], FD_CLOEXEC) < 0) return -1;
-	if (fcntl(wfd[1], FD_CLOEXEC) < 0) return -1;
-
 	kern_return_t kret;
-	pid_t pid = fork();
+	pid_t pid = vfork();
 	if (pid > 0) {
 		// In parent.
 		kret = acquire_mach_task(pid, task, port_set, exception_port, notification_port);
 		if (kret != KERN_SUCCESS) return -1;
-
-		char w;
-		read(wfd[0], &w, 1);
 
 		return pid;
 	}
